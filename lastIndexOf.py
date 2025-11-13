956a749db56df5b6d60f0e4819047bdde0df28f1
krzysztofkaptur <krzysztof.kaptur@gmail.com>
2025-11-13 18:56:10 +0100

    chore: add docs to lastIndexOf

--- lastIndexOf.py
+++ lastIndexOf.py
@@ -1,4 +1,16 @@
 def lastIndexOf(arr, value, fromIndex = None):
+    """
+    Gets the index at which the first occurrence of value is found in list except that it iterates over elements of list from right to left.
+
+    Args:
+        arr (list): The list to inspect.
+        value (any): The value to search for.
+        fromIndex (int, optional): The index to search from. Defaults to 0.
+
+    Returns:
+        (Any): Returns the last element of list.
+    """
+        
     result = None
     end = len(arr) - 1
     start = fromIndex if fromIndex is not None else end
