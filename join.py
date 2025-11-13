6e4397025fd40a8c2b7606c9d198528e9cbee673
krzysztofkaptur <krzysztof.kaptur@gmail.com>
2025-11-13 17:55:23 +0100

    feat: add join

---
+++ join.py
@@ -0,0 +1,15 @@
+def join(arr, separator = ","):
+    """
+    Converts all elements in list into a string separated by separator.
+
+    Args:
+        arr (list): The list to convert.
+        separator: (string, optional): The element separator. Defaults to ','.
+
+    Returns:
+        (string): Returns the joined string.
+    """
+
+    return f"{separator}".join(arr)
+
+print(join(['a', 'b', 'c'], '~')) # 'a~b~c'
