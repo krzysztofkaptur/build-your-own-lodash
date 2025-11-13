255bcead9f6d03819e0ec3862d88857f18e34c2a
krzysztofkaptur <krzysztof.kaptur@gmail.com>
2025-11-13 09:45:49 +0100

    fix: add missing fromIndex to find

--- find.py
+++ find.py
@@ -1,4 +1,4 @@
-def find(collection, predicate):
+def find(collection, predicate, fromIndex = 0):
     """
     Iterates over elements of collection, returning the first element predicate returns truthy for. The predicate is invoked with three arguments: (value, index|key, collection).
 
@@ -12,31 +12,36 @@ def find(collection, predicate):
     """
     result = None
 
-    for item in collection:
-        if predicate(item):
-            result = item
+    for i in range(fromIndex, len(collection)):
+        if predicate(collection[i]):
+            result = collection[i]
             break
 
     return result
 
 # For testing
-# users = [
-#   { 'user': 'barney',  'age': 36, 'active': True },
-#   { 'user': 'fred',    'age': 40, 'active': False },
-#   { 'user': 'pebbles', 'age': 1,  'active': True }
-# ]
+users = [
+  { 'user': 'barney',  'age': 36, 'active': True },
+  { 'user': 'fred',    'age': 40, 'active': False },
+  { 'user': 'pebbles', 'age': 1,  'active': True }
+]
 
-# def test1(o):
-#     return o["age"] < 40
+def test1(o):
+    return o["age"] < 40
 
-# print(find(users, test1)) # dict for 'barney'
+print(find(users, test1)) # dict for 'barney'
 
-# def test2(o):
-#     return o["age"] == 1 and o["active"] == True
+def test2(o):
+    return o["age"] == 1 and o["active"] == True
 
-# print(find(users, test2)) # dict for 'pebbles'
+print(find(users, test2)) # dict for 'pebbles'
 
-# def test3(o):
-#     return o["active"] == False
+def test3(o):
+    return o["active"] == False
 
-# print(find(users, test3)) # dict for 'fred'
+print(find(users, test3)) # dict for 'fred'
+
+def test4(o):
+    return o["age"] < 40
+
+print(find(users, test4, 1)) # dict for 'pebbles'
