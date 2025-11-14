5d5bc33f68b905791233f2cab556a81fbb5664b5
krzysztofkaptur <krzysztof.kaptur@gmail.com>
2025-11-14 19:58:13 +0100

    feat: add tail

---
+++ tail.py
@@ -0,0 +1,15 @@
+def tail(arr):
+    """
+    Gets all but the first element of list.
+
+    Args:
+        arr (list): The list to query.
+
+    Returns:
+        (list): Returns the slice of list.
+    """
+
+    return arr[1:]
+
+# For testing
+# print(tail([1, 2, 3])) # [2, 3]
